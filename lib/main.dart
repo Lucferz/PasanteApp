6a6d71e4929a81342515031abd8ae786ab9bc9d5
Lucas <lucasfrutoszarate@hotmail.com>
2022-12-13 07:13:49 -0400

    Cambios en el main para usar providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:pasante_app/services/services.dart';
 import 'package:pasante_app/views/views.dart';
+import 'package:provider/provider.dart';
  
-void main() => runApp(MyApp());
+void main() => runApp(AppState());
  
+
+class AppState extends StatelessWidget {
+   const AppState({Key? key}) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService())
+      ],
+      child: MyApp(),
+     );
+   }
+ }
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
---
+++ lib/services/services.dart
@@ -0,0 +1,2 @@
+export 'package:pasante_app/services/auth_service.dart';
+export 'package:pasante_app/services/notification_service.dart';
